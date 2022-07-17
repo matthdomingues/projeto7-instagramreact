a13833080c0cb5f057973767eb45d19ef980499c
root <root@DESKTOP-QJ6ICDP.localdomain>
2022-07-17 18:43:15 -0300

    Mapeamento de Itens Dinâmicos Completo

--- src/Esquerda.js
+++ src/Esquerda.js
@@ -2,137 +2,122 @@ export default function Esquerda() {
 
     return (
         <div class="esquerda">
-
-            <div class="stories">
-
-                <Stories img="assets/img/9gag.svg"
-                    usuario="9gag" />
-
-                <Stories img="assets/img/meowed.svg"
-                    usuario="meowed" />
-
-                <Stories img="assets/img/barked.svg"
-                    usuario="barked" />
-
-                <Stories img="assets/img/nathanwpylestrangeplanet.svg"
-                    usuario="nathanwpylestrangeplanet" />
-
-                <Stories img="assets/img/wawawicomics.svg"
-                    usuario="9gwawawicomicsag" />
-
-                <Stories img="assets/img/respondeai.svg"
-                    usuario="respondeai" />
-
-                <Stories img="assets/img/filomoderna.svg"
-                    usuario="filomoderna" />
-
-                <Stories img="assets/img/memeriagourmet.svg"
-                    usuario="memeriagourmet" />
-
-                <div class="setinha">
-                    <ion-icon name="chevron-forward-circle"></ion-icon>
-                </div>
-            </div>
-
-            <div class="posts">
-
-                <div class="post">
-                    <PostTopo img="assets/img/meowed.svg"
-                        usuario="meowed"
-                        ion="ellipsis-horizontal" />
-
-                    <PostConteudo img="assets/img/gato-telefone.svg" />
-
-                    <PostFundo
-                        ion1="heart-outline"
-                        ion2="chatbubble-outline"
-                        ion3="paper-plane-outline"
-                        ion4="bookmark-outline"
-
-                        img="assets/img/respondeai.svg"
-                        texto1="respondeai"
-                        texto2="101.523 pessoas"
-                    />
-                </div>
-
-                <div class="post">
-                    <PostTopo img="assets/img/barked.svg"
-                        usuario="barked"
-                        ion="ellipsis-horizontal" />
-
-                    <PostConteudo img="assets/img/dog.svg" />
-
-                    <PostFundo
-                        ion1="heart-outline"
-                        ion2="chatbubble-outline"
-                        ion3="paper-plane-outline"
-                        ion4="bookmark-outline"
-
-                        img="assets/img/respondeai.svg"
-                        texto1="adorable_animals"
-                        texto2="outras 99.159 pessoas"
-                    />
-                </div>
-
-            </div>
+            <Stories />
+            <Posts />
         </div>
 
     )
 
 }
 
-function Stories(props) {
+function Stories() {
+
+    const story = [{ usuario: "9gag", imagem: "assets/img/9gag.svg" },
+    { usuario: "meowed", imagem: "assets/img/meowed.svg" },
+    { usuario: "barked", imagem: "assets/img/barked.svg" },
+    { usuario: "nathanwpylestrangeplanet", imagem: "assets/img/nathanwpylestrangeplanet.svg" },
+    { usuario: "wawawicomics", imagem: "assets/img/wawawicomics.svg" },
+    { usuario: "respondeai", imagem: "assets/img/respondeai.svg" },
+    { usuario: "filomoderna", imagem: "assets/img/filomoderna.svg" },
+    { usuario: "memeriagourmet", imagem: "assets/img/memeriagourmet.svg" }]
+
+    function Story(props) {
+        return (
+            <div class="story">
+                <div class="imagem">
+                    <img src={props.imagem} />
+                </div>
+                <div class="usuario">
+                    {props.usuario}
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <div class="story">
-            <div class="imagem">
-                <img src={props.img} />
-            </div>
-            <div class="usuario">{props.usuario}</div>
-        </div>
-    )
-}
-
-function PostTopo(props) {
-    return (
-        <div class="topo">
-            <div class="usuario">
-                <img src={props.img} />
-                {props.usuario}
-            </div>
-            <div class="acoes">
-                <ion-icon name={props.ion}></ion-icon>
+        <div class="stories">
+            {story.map(st => <Story usuario={st.usuario} imagem={st.imagem} />)}
+            <div class="setinha">
+                <ion-icon name="chevron-forward-circle"></ion-icon>
             </div>
         </div>
     )
 }
 
-function PostConteudo(props) {
-    return (
-        <div class="conteudo">
-            <img src={props.img} />
-        </div>
-    )
-}
+function Posts() {
 
-function PostFundo(props) {
-    return (
-        <div class="fundo">
-            <div class="acoes">
-                <div>
-                    <ion-icon name={props.ion1}></ion-icon>
-                    <ion-icon name={props.ion2}></ion-icon>
-                    <ion-icon name={props.ion3}></ion-icon>
+    const post = [{
+        usuario: "meowed", imgUsuario: "assets/img/meowed.svg",
+        imgPost: "assets/img/gato-telefone.svg", curtidoPor: "respondeai",
+        imgCurtidoPor: "assets/img/respondeai.svg", qtdCurtidas: "101.523"
+    },
+    {
+        usuario: "barked", imgUsuario: "assets/img/barked.svg",
+        imgPost: "assets/img/dog.svg", curtidoPor: "adorable_animals",
+        imgCurtidoPor: "assets/img/adorable_animals.svg", qtdCurtidas: "99.159"
+    }]
+
+    function PostIndividual(props) {
+        return (
+            <div class="post">
+                <div class="topo">
+                    <div class="usuario">
+                        <img src={props.imgUsuario} />
+                        {props.usuario}
+                    </div>
+                    <div class="acoes">
+                        <ion-icon name="ellipsis-horizontal"></ion-icon>
+                    </div>
                 </div>
-                <div>
-                    <ion-icon name={props.ion4}></ion-icon>
+
+                <div class="conteudo">
+                    <img src={props.imgPost} />
+                </div>
+
+                <div class="fundo">
+                    <div class="acoes">
+                        <div>
+                            <ion-icon name="heart-outline"></ion-icon>
+                            <ion-icon name="chatbubble-outline"></ion-icon>
+                            <ion-icon name="paper-plane-outline"></ion-icon>
+                        </div>
+                        <div>
+                            <ion-icon name="bookmark-outline"></ion-icon>
+                        </div>
+                    </div>
+
+                    <div class="curtidas">
+                        <img src={props.imgCurtidoPor} />
+                        <div class="texto">
+                            Curtido por <strong>{props.curtidoPor}</strong> e <strong>outras {props.qtdCurtidas} pessoas</strong>
+                        </div>
+                    </div>
                 </div>
             </div>
+        )
+    }
 
-            <div class="curtidas">
-                <img src={props.img} />
-                <div class="texto">Curtido por <strong>{props.texto1}</strong> e <strong>{props.texto2}</strong></div>
-            </div>
+
+
+    return (
+        <div class="posts">
+
+            {post.map(p => (
+                <PostIndividual usuario={p.usuario}
+                    imgUsuario={p.imgUsuario}
+                    imgPost={p.imgPost}
+                    curtidoPor={p.curtidoPor}
+                    imgCurtidoPor={p.imgCurtidoPor}
+                    qtdCurtidas={p.qtdCurtidas}
+                    ion1={p.ion1} />))}
         </div>
     )
+
 }
+
+
+
+
+
+
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,45 +3,11 @@ export default function Sidebar() {
     return (
         <div class="sidebar">
 
-            <div class="usuario">
-                <img src="assets/img/catanacomics.svg" />
-                <div class="texto">
-                    <strong>catanacomics</strong>
-                    Catana
-                </div>
-            </div>
+            <Usuario img="assets/img/catanacomics.svg"
+                usuario1="catanacomics"
+                usuario2="catana" />
 
-            <div class="sugestoes">
-                <div class="titulo">
-                    Sugestões para você
-                    <div>Ver tudo</div>
-                </div>
-
-                <Sugestao
-                    img="assets/img/bad.vibes.memes.svg"
-                    nome="bad.vibes.memes"
-                    razao="Segue você" />
-
-                <Sugestao
-                    img="assets/img/chibirdart.svg"
-                    nome="chibirdart"
-                    razao="Segue você" />
-
-                <Sugestao
-                    img="assets/img/razoesparaacreditar.svg"
-                    nome="razoesparaacreditar"
-                    razao="Novo no Instagram" />
-
-                <Sugestao
-                    img="assets/img/adorable_animals.svg"
-                    nome="adorable_animals"
-                    razao="Segue você" />
-
-                <Sugestao
-                    img="assets/img/smallcutecats.svg"
-                    nome="smallcutecats"
-                    razao="Segue você" />
-            </div>
+            <Sugestoes />
 
             <div class="links">
                 Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
@@ -55,18 +21,67 @@ export default function Sidebar() {
 
 }
 
-function Sugestao(props) {
-    return (
-        <div class="sugestao">
-            <div class="usuario">
-                <img src={props.img} />
-                <div class="texto">
-                    <div class="nome">{props.nome}</div>
-                    <div class="razao">{props.razao}</div>
-                </div>
-            </div>
+function Usuario(props) {
 
-            <div class="seguir">Seguir</div>
+    return (
+        <div class="usuario">
+            <img src={props.img} />
+            <div class="texto">
+                <strong>{props.usuario1}</strong>
+                {props.usuario2}
+            </div>
         </div>
+    )
+}
+
+function Sugestoes() {
+
+    const sugest = [{
+        img: "assets/img/bad.vibes.memes.svg",
+        nome: "bad.vibes.memes",
+        razao: "Segue você"
+    }, {
+        img: "assets/img/chibirdart.svg",
+        nome: "chibirdart",
+        razao: "Segue você"
+    }, {
+        img: "assets/img/razoesparaacreditar.svg",
+        nome: "razoesparaacreditar",
+        razao: "Novo no Instagram"
+    }, {
+        img: "assets/img/adorable_animals.svg",
+        nome: "adorable_animals",
+        razao: "Segue você"
+    }, {
+        img: "assets/img/smallcutecats.svg",
+        nome: "smallcutecats",
+        razao: "Segue você"
+    }]
+
+    function Sugestao(props) {
+        return (
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src={props.img} />
+                    <div class="texto">
+                        <div class="nome">{props.nome}</div>
+                        <div class="razao">{props.razao}</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+        )
+    }
+    return (
+        <div class="sugestoes">
+            <div class="titulo">
+                Sugestões para você
+                <div>Ver tudo</div>
+            </div>
+            {sugest.map(s => (<Sugestao img={s.img} nome={s.nome} razao={s.razao} />))}
+
+        </div>
+
     )
 }
